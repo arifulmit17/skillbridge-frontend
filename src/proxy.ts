6ca42b98eb1d8e0ef8c898f63775c68aeb2562ba
Ariful <arifulmit17@gmail.com>
2026-01-30 14:48:55 +0600

    added proxy file

---
+++ src/proxy.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from './constants/roles';
+
+
+
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  let isAuthenticated = false;
+  let isAdmin = false;
+  let isTutor = false;
+  let isStudent = false;
+
+  const { data } = await userService.getSession();
+
+  if (data) {
+    isAuthenticated = true;
+    isAdmin = data.user.role === Roles.admin;
+    isTutor= data.user.role===Roles.tutor;
+    isStudent= data.user.role===Roles.student;
+  }
+
+  //* User in not authenticated at all
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  //* User is authenticated and role = ADMIN
+  //* User can not visit user dashboard
+  if (isAdmin && !pathname.startsWith("/admin-dashboard")) {
+    return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+  }
+
+  //* User is authenticated and role = USER
+  //* User can not visit admin-dashboard
+  if (!isAdmin && !isTutor && pathname.startsWith("/admin-dashboard")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+  if (!isAdmin && !isStudent && pathname.startsWith("/admin-dashboard")) {
+    return NextResponse.redirect(new URL("/tutor-dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/dashboard",
+    "/dashboard/:path*",
+    "/admin-dashboard",
+    "/admin-dashboard/:path*",
+    "/tutor-dashboard",
+    "/tutor-dashboard/:path*",
+  ],
+};
